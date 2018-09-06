5ff3fdb67a6baf7c673f10b33267ec679892cd59
theBashShell <github.com/theBashShell>
2018-09-06 15:34:08 +0000

    cat file renamed appropriately

--- lab1/my-cat.c
+++ lab1/my-cat.c
@@ -5,30 +5,44 @@
  * GitHub: 	https://github.com/theBashShell
  * Date: Sep-06-18
  * */
-
-
+ 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {
+	
+	// Required Checks
+	if(argc < 2) {
+		printf("Missing filename(s)!\n");
+		return EXIT_FAILURE;
+	}
+	
+	for(int i = 1; i < argc; i++) {
+		
 	// Open file
-	FILE* file_to_open = fopen(argv[1], "r");
+	FILE* file_to_open = fopen(argv[i], "r");
 	
 	if(!file_to_open) {
-		perror("Unable to open file! :(");
+		printf("Unable to open file: '%s'\n", argv[i]);
+		printf("File probably doesn't exist\n");
 		return EXIT_FAILURE;
 	}	
-	
-	
+		
 	// Print File Content
 	int check;		// track EOF
 	while((check = fgetc(file_to_open)) != EOF) {
 		putchar(check);		
 	}
 	
+	printf("\n\n");	// separated the files
+	
 	if(ferror(file_to_open)) {
-		puts("Reading Error Encountered");
+		puts("Error Encountered");
 	}
 	
 	fclose(file_to_open);
+	
+	}
+	
 }
+
